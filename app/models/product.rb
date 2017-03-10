9c78f05e37a8b3a3bac60b82e303c79fc777f0e7
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2017-03-10 14:02:28 -0300

    Improve readability with one validation per line

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
-  validates :name, :quantity, :price, presence: true
-  validates :price, numericality: true
+  validates :name, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true, numericality: true
 end
