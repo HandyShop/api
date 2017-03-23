062a2cb6e0285f38ef501d8d8ded33f0c3d7ffc5
Daniel Vasconcelos <daniel.olivervas@gmail.com>
2017-03-23 14:43:05 -0300

    Add numericality validation to product's quantity #6

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class Product < ApplicationRecord
   validates :name, presence: true
-  validates :quantity, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true }
   validates :price, presence: true, numericality: true
 
   belongs_to :market
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_presence_of :quantity }
   it { is_expected.to validate_presence_of :price }
   it { is_expected.to validate_numericality_of :price }
+  it { is_expected.to validate_numericality_of(:quantity).only_integer }
   it { is_expected.to belong_to :market }
 end
